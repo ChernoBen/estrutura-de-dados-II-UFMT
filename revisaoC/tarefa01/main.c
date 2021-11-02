d398b35ac0eacf162a4f5fd52cf19498bdce9d1f
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-11-02 13:23:34 -0400

    tarefa 01

--- revisaoC/tarefa01/main.c
+++ revisaoC/tarefa01/main.c
@@ -4,12 +4,26 @@
 
 int main(){
     Lista  *ls = criaLista();
-    //Questão número-1
+    //Questão número 1:
+
     ls = insereFim(ls,10);
     ls = insereFim(ls,9);
     ls = insereFim(ls,8);
     ls = insereFim(ls,7);
     imprimeLista(ls);
     
-    
+    // número 2:
+    ls = insere_ordenado_recursivo(ls,ls,19);
+    ls = insere_ordenado_recursivo(ls,ls,18);
+    ls = insere_ordenado_recursivo(ls,ls,11);
+    ls = insere_ordenado_recursivo(ls,ls,4);
+    imprimeLista(ls);
+
+    // número 3:
+    ls = exclui(ls,19);
+    imprimeLista(ls);
+
+    // número 4:
+    ls = altera(ls,11,10);
+    imprimeLista(ls);  
 }
--- revisaoC/tarefa01/utils.c
+++ revisaoC/tarefa01/utils.c
@@ -13,13 +13,38 @@ typedef struct lista{
 Lista *criaLista();
 Lista *insereFim(Lista *lista,int vl);
 void imprimeLista(Lista *lista);
+Lista *insere_ordenado_recursivo(Lista *lista,Lista *ant,int vl);
+Lista *exclui(Lista *lista, int valor);
+Lista *altera(Lista *lista, int valor,int novo_valor);
 //-------implementações
 
 //metodo para criar lista
 Lista *criaLista(){
     return NULL;
 }
-
+//metodo que insere de forma ordenada recursivamente
+Lista *insere_ordenado_recursivo(Lista *lista,Lista *ant,int vl){
+    //lp lista -> proxima posicao
+    //la lista valor anterior
+    Lista *lp = lista;
+    Lista *la = ant;
+    if (lp!=NULL && lp->valor < vl){
+        la = lp;
+        lp->prox = insere_ordenado_recursivo(lp->prox,la,vl);
+    }else{
+        Lista *novo_no = (Lista*)malloc(sizeof(Lista));
+        novo_no->valor = vl;
+        if(la==NULL){
+            novo_no->prox = la;
+            la = novo_no;
+        }else{
+            novo_no->prox = la->prox;
+            la->prox = novo_no;
+        }
+        return novo_no;
+    }
+    return lp;
+}
 //metodo que insere recursivamente um valo no final da lista
 Lista *insereFim(Lista *lista,int vl){
     Lista *ls = lista;
@@ -48,5 +73,38 @@ void imprimeLista(Lista *lista){
     }
 }
 
+//metodo que exclui um elemento da lista
+Lista *exclui(Lista *lista, int valor){
+    Lista *ls = lista;
+    if(lista->valor == valor){
+        lista = lista->prox;
+        free(ls);
+        return lista;
+    }
+    Lista *anterior;
+    while (ls != NULL){
+        anterior = ls;
+        ls = ls->prox;
+        if(ls && ls->valor == valor){
+            anterior->prox = ls->prox;
+            free(ls);
+            return lista;
+        }
+    }
+    printf("valor não existe.\n");
+    return lista;
+}
 
+//metodo que altera um valor em determinada posicao da lista
+Lista *altera(Lista *lista, int valor,int novo_valor){
+    Lista *ls = lista;
+    while(ls!=NULL){
+        if(ls->valor==valor){
+           ls->valor = novo_valor;
+           return lista;
+        }
+        ls = ls->prox;
+    }
+    printf("Valor não encontrado\n"); 
+}
 #endif
